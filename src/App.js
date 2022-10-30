f56cc63c59726a4a81e8612632b9543c7b074cfa
Marco Soares Coutinho <87444547+coutinhomarco@users.noreply.github.com>
2022-10-30 17:25:28 -0300

    cors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   setTimeout(async () => {
-    await fetch('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
+    await fetch('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json', { mode: 'no-cors'})
       .then(response => response.json())
       .then(data => {
         setData({data: data.cand, time: data.ht, totalapurado: data.psi})
@@ -15,7 +15,7 @@ function App() {
   }, 10000);
   useEffect(() => {
     const asyncF = async () => {
-      return fetch('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
+      return fetch('https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json', { mode: 'no-cors'})
       .then(response => response.json())
       .then(data => {
         setData({data: data.cand, time: data.ht, totalapurado: data.pst})
